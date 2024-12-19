4c801c1a138363b79d549652b5a322b772cb6ddd
fang <fangzhan1109@163.com>
2024-12-19 14:49:38 +0800

    workspace init

--- PandaYouxuan-admin/src/icons/index.ts
+++
@@ -1,5 +0,0 @@
-import SvgIcon from '@/components/SvgIcon/index.vue';// svg component
-
-import.meta.glob('./svg/*.svg', { eager: true });
-
-export default SvgIcon;
--- PandaYouxuan-admin/src/types/element-plus.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FormValidateCallback, FormValidationResult } from 'element-plus';
-
-interface IForm {
-  validate: (callback?: FormValidateCallback | undefined) => FormValidationResult;
-}
